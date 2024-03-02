ebd47bca71973a474ab5116fa981579e848933df
Nicolas Paxao <nicolaspaxao@gmail.com>
2024-03-01 23:35:39 -0300

    Testando autenticador de biometria

--- android/app/src/main/kotlin/br/com/nickdev/bank_app/MainActivity.kt
+++ android/app/src/main/kotlin/br/com/nickdev/bank_app/MainActivity.kt
@@ -1,6 +1,6 @@
 package br.com.nickdev.bank_app
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,23 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() {
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:local_auth/local_auth.dart';
+
+final LocalAuthentication _localAuth = LocalAuthentication();
+
+bool isBiometricAvaiable = false;
+bool canAuthenticate = false;
+
+Future<bool> checkBiometric() async {
+  return await _localAuth.canCheckBiometrics;
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  isBiometricAvaiable = await checkBiometric();
+  canAuthenticate =
+      (isBiometricAvaiable || await _localAuth.isDeviceSupported());
   runApp(const MainApp());
 }
 
@@ -10,9 +27,63 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuthenticated = false;
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  Future<bool> authBio() async {
+    try {
+      return await _localAuth.authenticate(
+        localizedReason:
+            'Precisamos validar sua digital para acessar os recursos.',
+        options: const AuthenticationOptions(
+          useErrorDialogs: true,
+          stickyAuth: true,
+          biometricOnly: true,
+        ),
+      );
+    } on PlatformException catch (e) {
+      ScaffoldMessenger(
+        child: SnackBar(
+          content: Text(e.toString()),
+        ),
+      );
+      return false;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            isAuthenticated = await authBio();
+
+            log(isAuthenticated.toString());
+
+            if (isAuthenticated) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(content: Text('Sucesso')),
+              );
+            }
+          },
+          child: const Text('Autenticar'),
         ),
       ),
     );
